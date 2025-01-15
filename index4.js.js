960f0c840e069811ab93e52fdd9a2941a44fd30b
Suraj Chauhan <chauhansuraj8970@gmail.com>
2025-01-15 05:52:04 +0530

    How to get first letter Capital in name
    How to get first letter Capital in name in easiest way

---
+++ index4.js.js
@@ -0,0 +1,8 @@
+
+let name = prompt("what is your name?");
+alert("hello "+" "+name);
+let firstChar = name.slice(0,1);
+toUpperCasefirstChar = firstChar.toUpperCase(0,1);
+let restofname = name.slice(1,name.length);
+let capitaliseName = toUpperCasefirstChar + restofname;
+alert("hello"+ " " +capitaliseName );
